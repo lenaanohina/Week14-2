cfd7202a584150e949dfc1d5cf6a3d6aef9b4776
Elena <elena.anohina.94@mail.ru>
2023-11-11 20:55:25 +0300

    task with an asterisk

--- assets/script/script.js
+++ assets/script/script.js
@@ -18,14 +18,14 @@ console.log("Максимальный балл:", maxGrade);
 let minGrade = Math.min(...grades);
 console.log("Минимальный балл:", minGrade);
 
-// Подсчёт и вывод количества студентов, получивших положительную оценку (60 и выше)
+// Подсчёт и вывод количества студентов, получивших положительную оценку
 let positiveGradesCount = grades.filter((grade) => grade >= 60).length;
 console.log(
   "Количество студентов с положительной оценкой:",
   positiveGradesCount
 );
 
-// Подсчёт и вывод количества студентов, получивших отрицательную оценку (ниже 60)
+// Подсчёт и вывод количества студентов, получивших отрицательную оценку
 let negativeGradesCount = grades.filter((grade) => grade < 60).length;
 console.log(
   "Количество студентов с отрицательной оценкой:",
@@ -48,3 +48,94 @@ let letterGrades = grades.map((grade) => {
 });
 
 console.log("Буквенные оценки:", letterGrades);
+
+// Задание со звёздочкой
+// Сохраняем массив режиссёров в переменную
+const directors = [
+  {
+    name: "Стивен Спилберг",
+    career: "Продюсер, Режиссёр, Актёр, Сценарист, Монтажёр",
+    films:
+      "https://ru.wikipedia.org/wiki/%D0%A4%D0%B8%D0%BB%D1%8C%D0%BC%D0%BE%D0%B3%D1%80%D0%B0%D1%84%D0%B8%D1%8F_%D0%A1%D1%82%D0%B8%D0%B2%D0%B5%D0%BD%D0%B0_%D0%A1%D0%BF%D0%B8%D0%BB%D0%B1%D0%B5%D1%80%D0%B3%D0%B0",
+    top_rated_film: "Список Шиндлера",
+  },
+  {
+    name: "Кристофер Нолан",
+    career:
+      "Сценарист, Продюсер, Режиссёр, Оператор, Монтажёр, Актёр, Композитор",
+    films:
+      "https://www.film.ru/compilation/vse-filmy-kristofera-nolana-ot-luchshego-k-horoshemu",
+    top_rated_film: "Начало",
+  },
+  {
+    name: "Мартин МакДона",
+    career: "Сценарист, Режиссёр, Продюсер",
+    films: "https://www.film.ru/person/martin-makdonah",
+    top_rated_film: "Три билборда на границе Эббинга, Миссури",
+  },
+  {
+    name: "Алексей Балабанов",
+    career: "Режиссёр, Сценарист, Актёр, Продюсер",
+    films: "https://www.film.ru/person/aleksey-balabanov",
+    top_rated_film: "Брат",
+  },
+  {
+    name: "Питер Фаррелли",
+    career: "Продюсер, Режиссёр, Сценарист, Актёр",
+    films: "https://www.film.ru/person/piter-farrelli",
+    top_rated_film: "Зелёная книга",
+  },
+  {
+    name: "Юрий Быков",
+    career: "Актёр, Режиссёр, Сценарист, Композитор, Монтажёр, Продюсер",
+    films: "https://www.film.ru/person/yuriy-bykov",
+    top_rated_film: "Дурак",
+  },
+  {
+    name: "Жан-Марк Валле",
+    career: "Режиссер, Продюсер, Монтажёр, Актёр, Сценарист",
+    films: "https://www.film.ru/person/zhan-mark-valle",
+    top_rated_film: "Далласский клуб покупателей",
+  },
+];
+// Вызываем метод forEach у массива
+directors.forEach(function (director) {
+  // В функцию-обработчик добавим код, который относится к работе с DOM
+  const nameElement = document.getElementById("name");
+  const careerElement = document.getElementById("career");
+  const filmsElement = document.createElement("p");
+
+  nameElement.textContent = director.name;
+  careerElement.textContent = director.career;
+  filmsElement.innerHTML =
+    '<a href="' +
+    director.films +
+    '" target="_blank">' +
+    "Фильмография" +
+    "</a>";
+  // topRatedFilmElement.textContent = "Лучший фильм: " + director.top_rated_film;
+
+  // Добавляем информацию на страницу
+  document.body.appendChild(nameElement);
+  document.body.appendChild(careerElement);
+  document.body.appendChild(filmsElement);
+  // document.body.appendChild(topRatedFilmElement);
+});
+
+// Создаем новый массив topFilmsList с лучшими фильмами режиссёров
+const topFilmsList = directors.map(function (director) {
+  return {
+    name: director.name,
+    top_rated_film: director.top_rated_film,
+  };
+});
+console.log(topFilmsList);
+
+const topFilms = document.createElement("h3");
+const topRatedFilmElement = document.createElement("p");
+
+topFilms.textContent = "Лучшие фильмы: ";
+topRatedFilmElement.textContent = topFilmsList;
+
+document.body.appendChild(topFilms);
+// document.body.appendChild(topFilmsList);
