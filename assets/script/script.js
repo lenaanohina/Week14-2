73a300e2552dd3d4ee02fe530d27db5e96263495
Elena <elena.anohina.94@mail.ru>
2023-11-14 19:13:42 +0300

    create of script.js and readme.md

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,39 +1,39 @@
 // Генерация случайных оценок для 12 студентов от 1 до 100
-let grades = Array.from(
+const grades = Array.from(
   { length: 12 },
   () => Math.floor(Math.random() * 100) + 1
 );
 console.log("Оценки:", grades);
 
 // Вычисление и вывод среднего балла студентов
-let averageGrade =
+const averageGrade =
   grades.reduce((total, grade) => total + grade) / grades.length;
 console.log("Средний балл студентов:", averageGrade.toFixed(2));
 
 // Поиск и вывод максимальной оценки среди студентов
-let maxGrade = Math.max(...grades);
+const maxGrade = Math.max(...grades);
 console.log("Максимальный балл:", maxGrade);
 
 // Поиск и вывод минимальной оценки среди студентов
-let minGrade = Math.min(...grades);
+const minGrade = Math.min(...grades);
 console.log("Минимальный балл:", minGrade);
 
 // Подсчёт и вывод количества студентов, получивших положительную оценку
-let positiveGradesCount = grades.filter((grade) => grade >= 60).length;
+const positiveGradesCount = grades.filter((grade) => grade >= 60).length;
 console.log(
   "Количество студентов с положительной оценкой:",
   positiveGradesCount
 );
 
 // Подсчёт и вывод количества студентов, получивших отрицательную оценку
-let negativeGradesCount = grades.filter((grade) => grade < 60).length;
+const negativeGradesCount = grades.filter((grade) => grade < 60).length;
 console.log(
   "Количество студентов с отрицательной оценкой:",
   negativeGradesCount
 );
 
 // Преобразование числовых оценок в буквенные оценки
-let letterGrades = grades.map((grade) => {
+const letterGrades = grades.map((grade) => {
   if (grade >= 80) {
     return "A";
   } else if (grade >= 60) {
